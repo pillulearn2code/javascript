dbc9c16d73521f313e0845688dc2b1beb646647e
myCode <ajanta.bisht@klarna.com>
2017-04-06 22:30:15 +0200

    first checkin

---
+++ types.js
@@ -0,0 +1,42 @@
+var myString = 'Ajanta';
+var myNumber = 18;
+var myBoolean = true;
+
+// Ajanta Work
+
+console.log('Name =' +myString);
+console.log('I am ' +myNumber ,'year old');
+
+// eperiments with Chris
+
+// The 3 different ways to write a function in JS
+/*var test = function (testArg) {
+	console.log("sdfghjklkjhgfsasdfg          " + testArg);
+}
+
+function test2 (testArg) {
+	console.log("sdfghjklkjhgfsasdfg          " + testArg);
+}
+
+// lambda
+var test3 = (testArg) => {
+	console.log("sdfghjklkjhgfsasdfg          " + testArg);
+}
+
+function test4 (no1, no2) {
+	return no1 + no2;
+}
+
+test(12345);
+
+test2(8765432);
+
+test3(348098656987654);
+*/
+
+// Do not edit the code under this line
+/*
+console.log("Name: " + myString);
+console.log("Lucky Number: " + myNumber);
+console.log("Good joke? " + myBoolean);
+*/
