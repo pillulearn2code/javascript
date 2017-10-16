3c14d2efcccee8a67decc0c3772a71c89d367855
Ajanta Bisht <ajanta.bisht@klarna.com>
2017-10-17 01:22:50 +0200

    new files added

--- Java-Script/AJAX/main.js
+++ Java-Script/AJAX/main.js
@@ -1,19 +1,26 @@
-var animalContainor = document.getElementById("animal-info");
+var animalContainer = document.getElementById("animal-info");
 var btn = document.getElementById("btn");
+
 btn.addEventListener("click", function() {
     var ourRequest = new XMLHttpRequest();
     ourRequest.open('GET', "https://learnwebcode.github.io/json-example/animals-1.json")
     ourRequest.onload = function() {
         var ourData = JSON.parse(ourRequest.responseText);
         renderHTML(ourData);
-        //console.log(ourRequest.responseText);
+        //console.log(ourData);
     };
     ourRequest.send();
 });
 
-function renferHTMLdata() {
-    animalContainor.insertAdjacentHTML("beforeend", "abcdef");
+function renderHTML(data) {
+    var htmlString = "";
+    console.log("first loop");
+    for (var i = 0; i < data.length; i++) {
+        console.log("second loop");
+        htmlString += "<p>" + data[i].name + " is a " + data[i].species + ".";
+    };
 
+    animalContainer.insertAdjacentHTML('beforeend', htmlString);
 
 }
 
